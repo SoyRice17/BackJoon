88ff50b5766134920a7dc33e3ce15bb850953ff3
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-05-04 23:22:36 +0900

    10988, 3003, 25083, 11718 문제풀이

---
+++ src/string/InEqualOut.java
@@ -0,0 +1,15 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//https://www.acmicpc.net/problem/11718
+public class InEqualOut {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String text = "";
+        while ((text = br.readLine()) != null){
+            System.out.println(text);
+        }
+    }
+}
