2e6f579e198c2303fbdbfd5230e23425baafa867
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-05-28 17:43:17 +0900

    27323, 1085, 3009, 15894, 9063, 10101, 5073, 14215 문제 풀이 기하 완료

---
+++ src/geometry/EscapeLine.java
@@ -0,0 +1,27 @@
+package geometry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+//https://www.acmicpc.net/problem/1085
+public class EscapeLine {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int u_X = Integer.parseInt(st.nextToken());
+        int u_Y = Integer.parseInt(st.nextToken());
+        int square_X = Integer.parseInt(st.nextToken());
+        int square_Y = Integer.parseInt(st.nextToken());
+
+        int mostMinLength = u_X > u_Y ? u_Y : u_X;
+
+        if (mostMinLength > square_X - u_X) {
+            mostMinLength = square_X - u_X;
+        }
+        if (mostMinLength > square_Y - u_Y) {
+            mostMinLength = square_Y - u_Y;
+        }
+        System.out.println(mostMinLength);
+    }
+}
