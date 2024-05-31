a2ce2d7c03385ed265f891d4308b86deb8d0dcda
SoyRice17 <rkswkdqkq6974@gmail.com>
2024-05-31 18:06:41 +0900

    24262, 24263, 24264 문제풀이

---
+++ src/timeComplexity/BIgONotation.java
@@ -0,0 +1,8 @@
+package timeComplexity;
+//https://www.acmicpc.net/problem/24262
+public class BIgONotation {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(0);
+    }
+}
---
+++ src/timeComplexity/BigONotation3.java
@@ -0,0 +1,14 @@
+package timeComplexity;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//https://www.acmicpc.net/problem/24264
+public class BigONotation3 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n*n);
+        System.out.println(2);
+    }
+}
